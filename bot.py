ed8b9b0552ab9bb5a4b8eafc5dd5909d4a2a7d2a
BKWN <76673539+BakwanHybrid@users.noreply.github.com>
2025-01-04 16:10:26 +0700

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,32 @@
+import discord
+from main import gen_pass
+import random
+
+
+# Variabel intents menyimpan hak istimewa bot
+intents = discord.Intents.default()
+# Mengaktifkan hak istimewa message-reading
+intents.message_content = True
+# Membuat bot di variabel klien dan mentransfernya hak istimewa
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'Logged in as {client.user}')
+ 
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('$halo'):
+        await message.channel.send("Hi!")
+    elif message.content.startswith('$bye'):
+        await message.channel.send("\U0001f642")
+    elif message.content.startswith('$gen_pass'):
+        await message.channel.send('Generated password:', gen_pass(10))
+    elif message.content.startswith('$flipcoin'):
+        await message.channel.send('Coin flipped in', random.choice(['tail','head']))
+    else:
+        await message.channel.send(message.content)
+
+client.run("MTMxOTk2MTYzODYwNjIxMzIxMg.Gj2P74.L4RjYh_uXqLWRcHSlXtzBrHGP8KP099mDXngiQ")
